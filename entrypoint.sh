1a4430e28af8a78c65b14aabf3471853da9c4c0a
luckdor <92556306+luckdor@users.noreply.github.com>
2023-08-29 10:50:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
-chown -R minio:minio /data
-/usr/bin/gosu minio /usr/bin/minio $@
+# chown -R minio:minio /data
+# /usr/bin/gosu minio /usr/bin/minio $@
+mc admin update
+
+server /data --console-address ":9090"
