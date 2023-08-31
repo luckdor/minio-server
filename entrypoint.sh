63c5f60f4d1e443b6b3dd376a03b78513bf8a5ba
luckdor <92556306+luckdor@users.noreply.github.com>
2023-08-31 10:13:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 # chown -R minio:minio /data
 # /usr/bin/gosu minio /usr/bin/minio $@
-# minio server /data --console-address :9090 --address :9001
+minio server /data --console-address ":9090" --address "wbwb-syw1738.koyeb.app:9001"
 
-docker-compose up -d
+# docker-compose up -d
 
 # service minio restart
 
